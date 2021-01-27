2f8a78d3c4aae9583a534a51ca6ca92d2a2559f6
Thiago Vidal <thiago.tmv@gmail.com>
2021-01-27 16:24:53 -0300

    Criando repository e service categoria

---
+++ src/main/java/com/thiago/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.thiago.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiago.cursomc.domain.Categoria;
+import com.thiago.cursomc.repositories.CategoriaRepository;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
